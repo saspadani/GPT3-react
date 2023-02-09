6e3ec47835340e9ba7cc334cee8a409135047a7b
saspadani <aspadani@gmail.com>
2023-02-09 22:33:10 +0700

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import {
+  Footer,
+  Blog,
+  Posstibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
+
 function App() {
-  return <h1>GPT-3</h1>;
+  return (
+    <div className='app'>
+      <div className='gradient__bg'>
+        <Navbar></Navbar>
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Posstibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
